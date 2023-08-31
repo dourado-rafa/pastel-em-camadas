473ef44fd126cb5edf238846acd19d5f7be5cb2c
beatrizrodriguesf <beatrizrf1@al.insper.edu.br>
2023-08-31 14:09:37 -0300

    Organizando os prints

--- Client-Server/client/client.py
+++ Client-Server/client/client.py
@@ -16,17 +16,16 @@ def main():
         com1.sendData(b'00')
         time.sleep(1)
  
-        print("Abriu a comunicação")
+        print("Abriu a comunicação\n")
 
         dic_comandos = {1: b'\x00\x00\x00\x00', 2:b'\x00\x00\xBB\x00', 3: b'\xBB\x00\x00', 4:b'\x00\xBB\x00', 5:b'\x00\x00\xBB', 6:b'\x00\xAA', 7:b'\xBB\x00', 8:b'\x00', 9:b'\xBB'}
         dic_tamanhos = {1: '0001', 2:'0010', 3:'0011', 4:'0100'}
         
         # Escreva o código aqui:
         n = random.randint(10,30)
-        print(f"serão enviados {n} comandos")
+        print(f"Serão enviados {n} comandos")
         comandos = np.random.randint(1,10, size=(n))
         num_comandos = len(comandos)
-        print(comandos)
 
         tamanhos = b''
         lista_comandos = b''
@@ -36,7 +35,9 @@ def main():
             concatenado = ''
 
             comando1_hex = dic_comandos[comandos[i]]
+            print(comando1_hex)
             comando2_hex = dic_comandos[comandos[i+1]]
+            print(comando2_hex)
             lista_comandos += comando1_hex + comando2_hex
 
             concatenado = dic_tamanhos[len(comando1_hex)] + dic_tamanhos[len(comando2_hex)]
@@ -45,7 +46,9 @@ def main():
 
         if num_comandos % 2 != 0:
             ultimo_comando = comandos[num_comandos-1]
-            len_ultimo = dic_tamanhos[len(dic_comandos[ultimo_comando])]
+            ultimo_comando_hex = dic_comandos[ultimo_comando]
+            print(ultimo_comando_hex)
+            len_ultimo = dic_tamanhos[len(ultimo_comando_hex)]
             tamanhos += int(len_ultimo,2).to_bytes(1, byteorder = 'big')
             
 
@@ -58,15 +61,15 @@ def main():
         print(txBuffer) 
         """
 
-        print("meu array de bytes tem tamanho {}" .format(len(txBuffer)))
+        print("Meu array de bytes tem tamanho {}\n" .format(len(txBuffer)))
 
-        print("iniciando transmissão")
+        print("Iniciando transmissão")
 
         com1.sendData(np.asarray(txBuffer))
         time.sleep(1)
 
         txSize = com1.tx.getStatus()
-        print('enviou = {}' .format(txSize))
+        print('Enviou {} bytes\n' .format(txSize))
 
         print("Esperando resposta do server")
         inicio = time.time()
@@ -76,20 +79,19 @@ def main():
         while time.time() - inicio <= 5 and not recebeu:
             rxBuffer_len = com1.rx.getBufferLen()
             if rxBuffer_len > 0:
-                print("recebeu {} bytes" .format(rxBuffer_len))
                 recebeu = True
                 rxBuffer, nRx = com1.getData(rxBuffer_len)
                 int_rxBuffer = int.from_bytes(rxBuffer, byteorder='big')
-                print(f"foram enviados {num_comandos} comandos e o servidor recebeu {int_rxBuffer} comandos")
+                print(f"Foram enviados {num_comandos} comandos e o servidor recebeu {int_rxBuffer} comandos")
                 if int_rxBuffer == num_comandos:
                     print("Tudo certo!")
                 else:
                     print("Algo deu errado")
 
         if not recebeu:
-            print("time out")
+            print("Time out :(")
         else:
-            print(f"Tempo de recepção = {time.time() - inicio}")
+            print(f"Tempo de recepção = {(time.time() - inicio)*1000:.2f} ms")
 
         # Encerra comunicação.
         print("\nComunicação encerrada")
