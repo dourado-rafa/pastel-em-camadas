303f74e4c805860b2d10febdb84ba866ecb0e96a
Rafael Dourado Bastos de Oliveira <103436880+dourado-rafa@users.noreply.github.com>
2023-09-14 14:58:58 -0300

    Corrigindo Datagrama

--- Point-to-Point/datagrama.py
+++ Point-to-Point/datagrama.py
@@ -1,22 +1,5 @@
 import numpy as np
-
-def head(type:int=0, server_number:int=0, current:int=0, total:int=0, id:int=0, restart:int=0, sucess:int=0) -> bytearray:
-    head = b''
-    head += type.to_bytes(1)
-    head += server_number.to_bytes(1)
-    head += b'\x00'
-    head += current.to_bytes(1)
-    head += total.to_bytes(1)
-    head += id.to_bytes(1)
-    head += restart.to_bytes(1)
-    head += sucess.to_bytes(1)
-    head += 3*b'\x00'
-    return head
-
-def datagrama(type:int=0, server_number:int=0, current:int=0, total:int=0, id:int=0, restart:int=0, sucess:int=0, 
-              payload:bytearray=b'', eop:str=b'\xAA\xBB\xCC\xDD') -> bytearray:
-    size_id = len(payload) if id != 0 else id
-    return np.asarray(head(type, server_number, current, total, size_id, restart) + payload + eop)
+from enlace_server.enlace import enlace
 
 def fragment_file(filepath:str, length:int=114) -> list[bytearray]:
     fragments = []
@@ -27,3 +10,47 @@ def fragment_file(filepath:str, length:int=114) -> list[bytearray]:
             fragments.append(fragment)
     file.close()
     return fragments
+
+def head(type:int=0, server_number:int=0, current:int=0, total:int=0, id:int=0, restart:int=0, sucess:int=0) -> bytearray:
+    head = b''
+    head += type.to_bytes(1)
+    head += server_number.to_bytes(1)
+    head += b'\x00'
+    head += total.to_bytes(1)
+    head += current.to_bytes(1)
+    head += id.to_bytes(1)
+    head += restart.to_bytes(1)
+    head += sucess.to_bytes(1)
+    head += 2*b'\x00'
+    return head
+
+def datagrama(type:int=0, server_number:int=0, current:int=0, total:int=0, id:int=0, restart:int=0, sucess:int=0, 
+              payload:bytearray=b'', eop:str=b'\xAA\xBB\xCC\xDD') -> bytearray:
+    size_id = len(payload) if id == 0 else id
+    return np.asarray(head(type, server_number, total, current, size_id, restart, sucess) + payload + eop)
+
+class Message():
+    def __init__(self, type:int=0, server_number:int=0, current:int=0, total:int=0, size_id:int=0, restart:int=0, sucess:int=0, payload:bytearray=b'', eop:bytearray=b'') -> None:
+        self.type = type
+        self.server_number = server_number
+        self.current = current
+        self.total = total
+        self.id = size_id if len(payload) == 0 else 0
+        self.size = size_id if len(payload) > 0 else 0
+        self.restart = restart
+        self.sucess = sucess
+        self.payload = payload
+        self.eop = eop
+        
+    def isValid(self) -> bool:
+        return (self.type == 0 and self.eop != b'\xAA\xBB\xCC\xDD')
+    
+def read_datagrama(head:bytearray, payload:bytearray, eop:bytearray) -> Message:
+    type = head[0]
+    server_number = head[1]
+    total = head[3]
+    current = head[4]
+    size_id = head[5]
+    restart = head[6]
+    sucess = head[7]
+    return Message(type, server_number, total, current, size_id, restart, sucess, payload, eop)
