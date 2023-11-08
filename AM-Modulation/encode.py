dcdb5d6d4098ad2346bbe022b4d8ea7786de5ad1
beatrizrodriguesf <beatrizrf1@al.insper.edu.br>
2023-11-08 13:51:48 -0300

    Encode com parâmetros do filtro calculados

--- AM-Modulation/encode.py
+++ AM-Modulation/encode.py
@@ -6,10 +6,25 @@ import matplotlib.pyplot as plt
 
 def main():
     
-    signal, samplerate = sf.read('audios/original.wav', dtype='float32') # Lendo o arquivo WAV original
+    signal, samplerate = sf.read('audios/signal.wav', dtype='float32') # Lendo o arquivo WAV signal
+    filtrado = []
+
+    a = 0.003285
+    b = 0.003108
+    c = 1
+    d = -1.84
+    e = 0.8465
+
+    filtrado.append(signal[0])
+    filtrado.append(signal[1])
+
+    for i in range(2,len(signal)):
+        valor = -d*filtrado[i-1] - e*filtrado[i-2] + a*signal[i-1] + b*signal[i-2]
+        filtrado.append(valor)
 
 
-    sf.write('audios/encoded.wav', signal, samplerate) # Criando arquivo WAV modulado
+    sf.write('audios/encoded.wav', filtrado, samplerate) # Criando arquivo WAV modulado
+
 
 if __name__ == "__main__":
     main()
