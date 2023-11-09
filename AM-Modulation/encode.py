139145043a271fa425cf87625945380131121415
beatrizrodriguesf <beatrizrf1@al.insper.edu.br>
2023-11-08 21:47:43 -0300

    Modulação e normalização do áudio feitas

--- AM-Modulation/encode.py
+++ AM-Modulation/encode.py
@@ -6,21 +6,49 @@ import matplotlib.pyplot as plt
 
 def main():
     
-    samplerate, signal = sf.read('audios/auau.wav') # Lendo o arquivo WAV signal
+    samplerate, signal = sf.read('audios/auau.wav') # Lendo o arquivo WAV channel1
+    channel1 = signal[:,0]
+
+    xf, yf = calcFFT(channel1, samplerate)
+    plt.plot(xf, np.abs(yf))
+
     filtrado = []
 
     a, b, c, d, e = 0.03365, 0.02783, 1, -1.504, 0.5656 # f_corte = 2000 
 
-    filtrado.append(signal[0])
-    filtrado.append(signal[1])
+    filtrado.append(channel1[0])
+    filtrado.append(channel1[1])
 
-    for i in range(2,len(signal)):
-        valor = -d*filtrado[i-1] - e*filtrado[i-2] + a*signal[i-1] + b*signal[i-2]
+    for i in range(2,len(channel1)):
+        valor = -d*filtrado[i-1] - e*filtrado[i-2] + a*channel1[i-1] + b*channel1[i-2]
         filtrado.append(valor)
 
+    xf, yf = calcFFT(filtrado, samplerate)
+    plt.plot(xf, np.abs(yf))
+    plt.show()
 
-    sf.write('audios/encoded.wav', samplerate, filtrado) # Criando arquivo WAV modulado
+    tempo, portadora = generateSin(14000, 5, samplerate)
+    S = []
+    for i in range(len(portadora)):
+        valor = (1+filtrado[i])*portadora[i]
+        S.append(valor)
 
+    plt.plot(tempo, S)
+    plt.show()
+
+    xf, yf = calcFFT(S, samplerate)
+    plt.plot(xf, np.abs(yf))
+    plt.show()
+
+    sinal_norm = []
+    maximo = max(S)
+    for valor in S:
+        sinal_norm.append(valor/maximo)
+
+    plt.plot(tempo, sinal_norm)
+    plt.show()
+
+    sf.write('audios/encoded.wav', samplerate, np.array(sinal_norm)) # Criando arquivo WAV modulado
 
 if __name__ == "__main__":
     main()
