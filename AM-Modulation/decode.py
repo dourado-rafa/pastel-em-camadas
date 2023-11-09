9f718888d507f094c8f7101414a315a5341e234a
dourado-rafa <rafaeldbo@al.insper.edu.br>
2023-11-08 23:10:19 -0300

    Decode Funcionando

--- AM-Modulation/decode.py
+++ AM-Modulation/decode.py
@@ -4,40 +4,46 @@ import sounddevice as sd
 from scipy.io import wavfile as sf
 import matplotlib.pyplot as plt
 
-def low_pass_filter(signal:np.ndarray) -> list:
-    # a, b, c, d, e = 0.1121, 0.07663, 1, -1.131, 0.33199 # f_corte = 4000 
-    # a, b, c, d, e = 0.05021, 0.03959, 1, -1.401, 0.4905 # f_corte = 2500 
+def low_pass_filter(signal:np.ndarray) -> list: 
     a, b, c, d, e = 0.03365, 0.02783, 1, -1.504, 0.5656 # f_corte = 2000 
 
     filtered_signal = [0, 0]
     for i in range(2, len(signal)):
         filtered_signal.append(-d*filtered_signal[i-1] -e*filtered_signal[i-2] + a*signal[i-1] + b*signal[i-2])
-    return filtered_signal
-        
+    return np.array(filtered_signal)
+
+f_carrier = 14e3 # Hz
+band = 4e3 # Hz
+duration = 5 # s
 
 def main():
     
     samplerate, signal = sf.read('audios/encoded.wav') # Lendo o arquivo WAV modulado
-    channel1 = signal[:, 0]
+    _, carrier_signal = generateSin(f_carrier, duration, samplerate)
+    time = np.linspace(0, duration, samplerate*duration)
     
-    channel1 = channel1/np.max(np.abs(channel1))
-    # sd.play(channel1, samplerate)
-    # sd.wait
-
-    freqs, ampls = calcFFT(channel1, samplerate)
-    # plt.figure()
+    demoduled_signal = carrier_signal*signal
+    freqs, ampls = calcFFT(demoduled_signal, samplerate)
+    plt.figure('Fourier - Sinal Demodulado')
     plt.plot(freqs, np.abs(ampls))
-    plt.xlim(0, 15e3)
+    plt.xlim(0, 20e3)
 
-    filtered_signal = low_pass_filter(channel1)
+    plt.figure('Sinal Demodulado')
+    plt.plot(time, demoduled_signal)
+    plt.xlim(0, 5)
+
+    filtered_signal = low_pass_filter(demoduled_signal)
     freqs, ampls = calcFFT(filtered_signal, samplerate)
-    # plt.figure()
+    plt.figure('Fourier - Sinal Demodulado e Filtrado')
     plt.plot(freqs, np.abs(ampls))
-    plt.xlim(0, 15e3)
+    plt.xlim(0, 20e3)
 
+    sd.play(filtered_signal, samplerate)
+    
     plt.show()
+    sd.wait()
 
-    sf.write('audios/decoded.wav', samplerate, channel1) # Criando arquivo WAV demodulado
+    sf.write('audios/decoded.wav', samplerate, filtered_signal) # Criando arquivo WAV demodulado
 
 if __name__ == "__main__":
     main()
